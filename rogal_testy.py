a56f05974276d4b8cbe86f4db681fe4c50de3c7b
Szymon Pluta <satanthesavior>
2019-02-26 14:44:52 +0100

    Clean code and refactor

--- rogal_testy.py
+++ rogal_testy.py
@@ -1,16 +1,18 @@
-
 import os
 import random
 import platform
 import items
-#import scisors2
+import scisors2
+
 try:
 	from msvcrt import getch 
 except ImportError:
 
 	def getch():
 
-		import sys, tty, termios
+		import sys
+		import tty
+		import termios
 		fd = sys.stdin.fileno()
 		old_settings = termios.tcgetattr(fd)
 		try:
@@ -19,6 +21,7 @@ except ImportError:
 		finally:
 			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 		return ch
+
 
 def pressedkey():
 	return getch()
@@ -48,86 +51,86 @@ room = {1:['#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','
 		21:['#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','#','.','.','.','.','.','.','.','#'],# |
 	    22:['#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#']}# |
 		# <--------------------------------------------y------------------------------------------------------->
-stuff  = {'wall'  :  "#",
+actors = {'wall'  :  "#",
 		  'player':  "@",
 		  'empty' :  ".",
 		  'money' :  "$",
 		  'chest' :  "C"}
-potion = {'hp': 10,'exp': 0,'def': 1,'atc': 1,'lvl': 1}
-print(potion)
-	
-invwntory = []
+stats = {'hp': 10,'exp': 0,'def': 1,'atc': 1,'lvl': 1}
+
+
+inventory = []
 otherlist = []
 pos = [] 
 
-def zbieranie():
-	for key in room:
-		print(room[key])# ta linijka wyswietla duza mape XD
+def pick_up():
+#	for key in room:
+#		print(room[key])# ta linijka wyswietla duza mape XD
 
 	if pos == [20, 2] and 'melon' in otherlist:
 		print()
 	elif pos == [20, 2] and 'melon' not in otherlist:
 		otherlist.append('melon')
-		invwntory.append('hp')
-		invwntory.append('hp')
-		add_to_inventory(potion, invwntory)
-		print("dodoano hp")
-		invwntory.clear()
+		inventory.append('hp')
+		inventory.append('hp')
+		add_to_inventory(stats, inventory)
+		print("added hp")
+		inventory.clear()
 
-	if pos == [15, 8] and 'sliwka' in otherlist:
+	if pos == [15, 8] and 'plum' in otherlist:
 		print()
-	elif pos == [15, 8] and 'sliwka' not in otherlist:
-		otherlist.append('sliwka')
-		invwntory.append('hp')
-		add_to_inventory(potion, invwntory)
-		print("dodoano hp")
-		invwntory.pop()
+	elif pos == [15, 8] and 'plum' not in otherlist:
+		otherlist.append('plum')
+		inventory.append('hp')
+		add_to_inventory(stats, inventory)
+		print("added hp")
+		inventory.pop()
 
-	if pos == [6, 20] and 'rekawice robocze' in otherlist:
+	if pos == [6, 20] and 'leather gloves' in otherlist:
 		print()
-	elif pos == [6, 20] and 'rekawice robocze' not in otherlist:
-		otherlist.append('rekawice robocze')
-		invwntory.append('def')
-		add_to_inventory(potion, invwntory)
-		print("dodoano def")
-		invwntory.pop()
+	elif pos == [6, 20] and 'leather gloves' not in otherlist:
+		otherlist.append('leather gloves')
+		inventory.append('def')
+		add_to_inventory(stats, inventory)
+		print("added def")
+		inventory.pop()
 
-	if pos == [7, 11] and 'zbroja wielkiego kutanga' in otherlist:
+	if pos == [7, 11] and 'Greater kahoot armor' in otherlist:
 		print()
-	elif pos == [7, 11] and 'zbroja wielkiego kutanga' not in otherlist:
-		otherlist.append('zbroja wielkiego kutanga')
-		invwntory.append('def')
-		invwntory.append('def')
-		add_to_inventory(potion, invwntory)
-		print("dodoano def")
-		invwntory.clear()
+	elif pos == [7, 11] and 'Greater kahoot armor' not in otherlist:
+		otherlist.append('Greater kahoot armor')
+		inventory.append('def')
+		inventory.append('def')
+		add_to_inventory(stats, inventory)
+		print("added def")
+		inventory.clear()
+	
+#################3
+	# TODO poprwić trza
+	for item in items.clothes():
+		if pos == [20, 16] and item in item.clothes() in otherlist:
+			print()
+		elif pos == [20, 16] and items.clothes() not in otherlist:
+			otherlist.append(items.clothes()[random.randint(0, len(items.clothes())-1)])
+			inventory.append('atc')
+			inventory.append('atc')
+			add_to_inventory(stats, inventory)
+			print("added atc")
+			inventory.clear()				
+#################
 
-	if pos == [20, 16] and 'miecz walusa' in otherlist:
+	if pos == [16, 20] and 'Nazgûl' in otherlist:
 		print()
-	elif pos == [20, 16] and 'miecz walusa' not in otherlist:
-		otherlist.append('miecz walusa')
-		invwntory.append('atc')
-		invwntory.append('atc')
-		add_to_inventory(potion, invwntory)
-		print("dodoano atc")
-		invwntory.clear()
+	elif pos == [16, 20] and 'Nazgûl' not in otherlist:
+		otherlist.append('Nazgûl')
+		scisors2.rock()
+		inventory.append('exp')
+		add_to_inventory(stats, inventory)
+		inventory.clear()
 
 
-	if pos == [16, 20] and 'nazgul' in otherlist:
-		print()
-	elif pos == [16, 20] and 'nazgul' not in otherlist:
-		otherlist.append('nazgul')
-		import scisors2
-		invwntory.append('exp')
-		add_to_inventory(potion, invwntory)
-		invwntory.clear()
-
-		
 ################################################################################
 ################################################################################
-
-
-
 
 
 def add_to_inventory(inventory, added_items):
@@ -139,20 +142,6 @@ def add_to_inventory(inventory, added_items):
 	return inventory
 
 
-'''
-	if stuff['player'] in room:
-		if stuff['player'] == pos[20, 2]:
-			print('jupi')
-	else:
-		print('grrrr')
-'''
-	#for pos == [20, 2] in room:
-		#if pos[stuff['player']] == pos[stuff['$']]:
-		#if stuff['player'] in pos[20, 2]:
-			#potion['hp'] == potion['hp'] + 2
-			#print(potion['hp'])
-
-
 ############################################################################################################
 ##########################################################################################################
 #########################################################################################################
@@ -162,9 +151,9 @@ def gamemap():
 
 def player_pos():
 	for i in range(1,len(room)+1):
-		if stuff['player'] in room[i]:
+		if actors['player'] in room[i]:
 			x_axis = i
-			y_axis = room[i].index(stuff['player'])
+			y_axis = room[i].index(actors['player'])
 			global pos
 			del pos[:]
 			pos.append(x_axis)
@@ -177,9 +166,9 @@ def updater():
 		os.system('clear')
 	gamemap()
 	player_pos()
-	zbieranie()
-	print(invwntory)
-	print(potion)
+	pick_up()
+	print(inventory)
+	print(stats)
 	print(otherlist)
  
 
@@ -212,58 +201,58 @@ def right(ditcioary,inst_replace,inst_player):
 
 
 
+def controls():
+	while True:
+		pressedkey = getch()
+		if pressedkey is 'w' or pressedkey is 'W':
+			if room[pos[0]-1][pos[1]] is not actors['wall']:
+				up(room, actors['empty'], actors['player'])
+				updater()
+				print(pos)
+		elif pressedkey is 's' or pressedkey is 'S':
+			if room[pos[0]+1][pos[1]] is not actors['wall']:
+				down(room,actors['empty'],actors['player'])
+				updater()
+				print(pos)
+		elif pressedkey is 'a' or pressedkey is 'A':
+			if room[pos[0]][pos[1]-1] is not actors['wall']:
+				left(room,actors['empty'], actors['player'])
+				updater()
+				print(pos)
+		elif pressedkey is 'd' or pressedkey is 'D':
+			if room[pos[0]][pos[1]+1] is not actors['wall']:
+				right(room,actors['empty'], actors['player'])
+				updater()
+				print(pos)
+
+
 #updater()
-gamemap()
+
+##############################################################################################
+##############################################################################################
+
+
+def print_character_statistics(char_stats):
+    string = "hp:{:}\tdef:{:}\tatc:{:}\texp:{:}\tlvl:{:}"
+    print(string.format(*char_stats.values()))
+
+def add_character_stats():
+	print_character_statistics(stats)
+	points = 5
+	while points > 0:
+		#while not is_answer_correct:
+		stat_to_add = input("Enter 'H', 'h', 'D', 'd' or 'A', 'a' to add statistic: ")
+		if stat_to_add in ["h", "H"]:
+			stats["hp"] += 2
+		elif stat_to_add in ["D", "d"]:
+			stats["def"] += 1
+		elif stat_to_add in ["A", "a"]:
+			stats["atc"] += 1
+		points -= 1
+		os.system("clear")
+		print_character_statistics(stats)
+
+
+add_character_stats()
 player_pos()
-
-
-
-
-while True:
-	pressedkey = getch()
-	if pressedkey is 'w' or pressedkey is 'W':
-		if room[pos[0]-1][pos[1]] is not stuff['wall']:
-			up(room, stuff['empty'], stuff['player'])
-			updater()
-			print(pos)
-	elif pressedkey is 's' or pressedkey is 'S':
-		if room[pos[0]+1][pos[1]] is not stuff['wall']:
-			down(room,stuff['empty'],stuff['player'])
-			updater()
-			print(pos)
-	elif pressedkey is 'a' or pressedkey is 'A':
-		if room[pos[0]][pos[1]-1] is not stuff['wall']:
-			left(room,stuff['empty'], stuff['player'])
-			updater()
-			print(pos)
-	elif pressedkey is 'd' or pressedkey is 'D':
-		if room[pos[0]][pos[1]+1] is not stuff['wall']:
-			right(room,stuff['empty'], stuff['player'])
-			updater()
-			print(pos)
-##############################################################################################
-##############################################################################################
-
-
-def get_string_input(question):
-    answer = input(question)
-
-    return answer
-
-def create_characterdef():
-    points = 5
-    while points > 0:
-        #while not is_answer_correct:
-        stat_to_add = get_string_input("Enter 'H', 'h', 'D', 'd' or 'A', 'a' to add statistic: ")
-        if stat_to_add in ["h", "H"]:
-            potion["hp"] += 2
-        elif stat_to_add in ["D", "d"]:
-            potion["def"] += 1
-        elif stat_to_add in ["A", "a"]:
-            potion["atc"] += 1
-        points -= 1
-    print('x')
-
-
-#create_characterdef()
-#print(potion)
+controls()
--- scisors2.py
+++ scisors2.py
@@ -4,28 +4,24 @@ from random import randint
 
 def rock():
 
-
-
-
-    #create a list of play options
-    #print("Type r for ROCK , p for PAPER , s for Scissors")
+    # create a list of play options
+    # print("Type r for ROCK , p for PAPER , s for Scissors")
     t = ["Rock", "Paper", "Scissors"]
 
-    #assign a random play to the computer
-    computer = t[randint(0,2)]
+    # assign a random play to the computer
+    computer = t[randint(0, 2)]
 
-    #set player to False
-    #player = False
-    
-    #licznik gier
-    how_many =[]
-    
+    # set player to False
+    # player = False
 
-    #while player == False:
+    # licznik gier
+    how_many = []
+
+    # while player == False:
     #  gramy 3 tury
-    while len(how_many) != 3:   
+    while len(how_many) != 3:
 
-    #set player to True
+        # set player to True
         player = input("Rock, Paper, Scissors?").capitalize()
         if player == computer:
             print("Tie!")
@@ -33,7 +29,7 @@ def rock():
             if computer == "Paper":
                 print("You lose!", computer, "covers", player)
                 how_many.append("L")
-                print(how_many) 
+                print(how_many)
             else:
                 print("You win!", player, "smashes", computer)
                 how_many.append("W")
@@ -43,7 +39,7 @@ def rock():
             if computer == "Scissors":
                 print("You lose!", computer, "cut", player)
                 how_many.append("L")
-                print(how_many) 
+                print(how_many)
             else:
                 print("You win!", player, "covers", computer)
                 how_many.append("W")
@@ -53,7 +49,7 @@ def rock():
             if computer == "Rock":
                 print("You lose...", computer, "smashes", player)
                 how_many.append("L")
-                print(how_many) 
+                print(how_many)
             else:
                 print("You win!", player, "cut", computer)
                 how_many.append("W")
@@ -61,26 +57,23 @@ def rock():
 
         else:
             print("That's not a valid play. Check your spelling!")
-        #player was set to True, but we want it to be False so the loop continues
-        #player = False
-        #rock()
-        computer = t[randint(0,2)]
-    
-    
-    if len(how_many)>=3:
-        # sadze ze zamiiast ponizszego warunku powinien byc warunek choc jednej wygranej w 3 turach poniewaz teraz jest za ciezko wygrac [ how_many.count("W") >= 1 ]
-        # mozna dopisac historie ze przeciwnik jest przekonany o swojej nieomylnosci i pojedyncza przegrana sprawia ze traci sens zycia XD
+        # player was set to True, but we want it to be False so the loop continues
+        # player = False
+        # rock()
+        computer = t[randint(0, 2)]
+
+    if len(how_many) >= 3:
+        # sadze ze zamiiast ponizszego warunku powinien byc warunek
+        # choc jednej wygranej w 3 turach poniewaz teraz jest za ciezko wygrac [ how_many.count("W") >= 1 ]
+        # mozna dopisac historie ze przeciwnik jest przekonany o swojej nieomylnosci i
+        # pojedyncza przegrana sprawia ze traci sens zycia XD
         if how_many.count("W") > how_many.count("L"):
             print("jestttttttttttttttttttt")
-            #file = open("congrats.txt",'r') 
-            #print (file.read())
+            # file = open("congrats.txt",'r')
+            # print (file.read())
             return True
         else:
             print("nieeeeeeeeeeeeee")
-            #file = open("lose.txt",'r') 
-            #print (file.read())
-            return False                
-
-    
-    
-rock()
+            # file = open("lose.txt",'r')
+            # print (file.read())
+            return False
